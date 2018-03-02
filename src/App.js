df7cd98d7d6f1787182289f171cd1b81faf70edb
eder-toledo <me890506@gmail.com>
2018-03-01 19:52:58 -0600

    Se agrego plugin para traducción y se agregaron las primeras pruebas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { Provider, Translate, Translator } from 'react-translated';
+import translation from './translation';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <Provider language="es" translation={translation}>
+        <div className="App">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-lg-12 col-md-12 col-sm-12">
+                <h2>KIDA Form Designer</h2>
+              </div>
+              <div class="col-lg-4 col-md-12 col-sm-12">
+                <Translate text='Formulario' />
+                <div></div>
+              </div>
+              <div class="col-lg-5 col-md-7 col-sm-12">
+                <Translate text='Agregar elemento' />
+                <div></div>
+              </div>
+              <div class="col-lg-3 col-md-5 col-sm-12">
+                <Translate text='Propiedades' />
+                <div></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Provider>
     );
   }
 }
---
+++ src/translation.js
@@ -0,0 +1,15 @@
+export default {
+  'Formulario': {
+    en: 'Form',
+    es: 'Formulario',
+  },
+  'Agregar elemento': {
+    en: 'Add element',
+    es: 'Agregar elemento',
+  },
+  'Propiedades': {
+    en: 'Properties',
+    es: 'Propiedades'
+  }
+  // ...
+}
